6972156506b34b9477735d8bb0c186421dcfe20c
rileyjson <riley.jamison@bsu.edu>
2025-02-17 01:00:13 -0500

    Add menu page content

--- src/menu-content.js
+++ src/menu-content.js
@@ -2,6 +2,26 @@
 
 export default function loadMenu() {
     
+    const content = document.querySelector("#content");
+    const menuDiv = document.createElement("div");
+
+    content.style.backgroundColor = "#D3D3D3"
+    content.style.height = "100vh";
+    content.style.cssText = "display: flex; gap: 20px; flex-wrap: wrap;"
+
     
+    for (let i=0; i<12; i++) {
+        const foodItem = document.createElement("div");
+        foodItem.className = `food ${i}`;
+        foodItem.style.width = "150px";
+        foodItem.style.height = "150px";
+        foodItem.style.backgroundColor = "white"
+        foodItem.style.csstext = "box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;"
+
+
+        content.appendChild(foodItem);
+    }
+
+
 
 }
